4e0b1eecebdc6780391d6968e124609f1ff0436b
Geo-red <geo.robin2025@vitstudent.ac.in>
2026-07-26 18:32:32 +0530

    Update 1.py

--- Assignment 1/1.py
+++ Assignment 1/1.py
@@ -1,1 +1,14 @@
+name = input("Enter student name: ")
 
+total = 0
+for i in range(1,6):
+    marks = float(input(f"Enter marks for Subject {i}: "))
+    total += marks
+
+average = total/5
+percentage = average
+
+print("Student Name:", name)
+print("Total:", total)
+print("Average:", average)
+print("Percentage:", percentage,"%")
