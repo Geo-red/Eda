ca4c283d0d24d03fe0fb24e162137a968cecdd28
Geo-red <geo.robin2025@vitstudent.ac.in>
2026-07-26 19:41:31 +0530

    Update 5.py

--- Assignment 3/5.py
+++ Assignment 3/5.py
@@ -1,1 +1,17 @@
+import pandas as pd
 
+df1 = pd.read_csv("Auto(1).csv")
+df2 = pd.read_csv("auto-mpg(1).csv")
+
+df2.rename(columns={
+    "model year": "year",
+    "car name": "name"
+}, inplace=True)
+
+left_join = pd.merge(df1, df2, on="mpg", how="left")
+
+print("Left Join")
+print(left_join)
+
+print("\nShape:")
+print(left_join.shape)
