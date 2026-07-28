ac040f82b9774531793342318e3c154098e70420
Geo-red <geo.robin2025@vitstudent.ac.in>
2026-07-28 12:06:41 +0530

    Update 3.py

--- Assignment 2/3.py
+++ Assignment 2/3.py
@@ -1,1 +1,5 @@
-
+import pandas as pd
+df = pd.read_csv("auto-mpg.csv")
+df["horsepower"] = pd.to_numeric(df["horsepower"], errors="coerce")
+df["Power_to_Weight"] = df["horsepower"] / df["weight"]
+print(df)
